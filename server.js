9abf147f41f5b38bebf2c1009d372aec36dcad67
DavorJ94 <davorjovanovic94@gmail.com>
2021-07-29 09:50:56 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data.json");
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middleware);
+server.use(router);
+server.listen(port);
